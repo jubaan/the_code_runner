fcb16785fb95e2a44a6a1699c94f82a2442cc678
jubaan <julio.ab@outlook.com>
2020-08-19 15:01:39 +0000

    Parenting_partnering read standar input

--- challenges/parenting_partering_returns/parenting_partnering_returns.rb
+++ challenges/parenting_partering_returns/parenting_partnering_returns.rb
@@ -1,3 +1,21 @@
-class ParentingPartneringReturns
+def parenting(test, schedule)
+  time_blocks = []
+  schedule.map do |t|
+    time_blocks << (t[0]..t[-1])
+  end
+  time_blocks
+end
 
+
+test_cases = gets.to_i
+
+test_cases.times do |test|
+  activities = gets.to_i
+  schedule = []
+
+  activities.times do
+    schedule << gets.chomp.split(' ').map(&:to_i)
+  end
+
+  parenting(test, schedule)
 end
