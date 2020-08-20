450c10a441db4d574b0e5f538a8250016eb4aa79
jubaan <julio.ab@outlook.com>
2020-08-20 16:01:16 +0000

    Improve overlaps? method and update the method everywhere it was called

--- challenges/parenting_partering_returns/parenting_partnering_returns.rb
+++ challenges/parenting_partering_returns/parenting_partnering_returns.rb
@@ -1,31 +1,35 @@
-# Divided by 60 to obtain the hours in a 12hr clock
-def overlaps?(array, element)
-    array.empty? ? (0..0).include?(element/60) : ((array.last[0]/60)...(array.last[-1]/60)).include?(element/60)
+require 'pry-byebug'
+
+def overlaps?(activity_1, activity_2)
+    return false unless activity_1
+    return false unless activity_2
+    [activity_1[0], activity_2[0]].max < [activity_1[-1], activity_2[-1]].min
 end
 
 def schedule(test, time_blocks)
-  cameron = []
-  jamie = []
-  output = ''
+     cameron = []
+     jamie = []
+     output = ''
 
-  i = 0
-  while i < time_blocks.length
-    if i == 0
-      cameron << time_blocks[i]
-      i += 1
-      next output << 'C'
-    elsif overlaps?(cameron, time_blocks[i][0]) && overlaps?(jamie, time_blocks[i][0]) || overlaps?(cameron, time_blocks[i][-1]) && overlaps?(jamie, time_blocks[i][-1])
-      break output = 'IMPOSSIBLE'
-    elsif overlaps?(cameron, time_blocks[i][0]) || overlaps?(cameron, time_blocks[i][-1])
-      jamie << time_blocks[i]
-      output << 'J'
-    else
-      cameron << time_blocks[i]
-      output << 'C'
+     i = 0
+     while i < time_blocks.length
+       # binding.pry
+        if i == 0
+            cameron << time_blocks[i]
+            i += 1
+            next output << 'C'
+        elsif overlaps?(cameron.last, time_blocks[i])  && overlaps?(jamie.last, time_blocks[i])
+            break output = 'IMPOSSIBLE'
+        elsif overlaps?(cameron.last, time_blocks[i])
+            jamie << time_blocks[i]
+            output << 'J'
+        else
+            cameron << time_blocks[i]
+            output << 'C'
+        end
+        i += 1
     end
-    i += 1
- end
-  "Case ##{test}: #{output}"
+    "Case ##{test}: #{output}"
 end
 
 # Gets input, and assigns it to variables
