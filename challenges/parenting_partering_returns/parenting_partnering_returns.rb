3a27462d7d841e4bd1d348137dba0798a8ff73d7
jubaan <julio.ab@outlook.com>
2020-08-20 20:16:25 +0000

    Update rubydoctest result

--- challenges/parenting_partering_returns/parenting_partnering_returns.rb
+++ challenges/parenting_partering_returns/parenting_partnering_returns.rb
@@ -1,7 +1,7 @@
 
 # doctest: Returns a string, "IMPOSSIBLE" if it cann't be solved or a string with the case number and one of the possible schedules
 # >> schedule(1, [[360, 480], [420, 540], [600,660]])
-# => "Case #1: CJC"
+# => "Case #1: JCJ"
 
 def overlap_any?(assigned_schedule, new_activity)
   assigned_schedule.any? do |activity|
@@ -14,28 +14,48 @@ def schedule(test, time_blocks)
   jamie = []
   output = ''
 
-  i = 0
-  while i < time_blocks.length
-    if i == 0
-      cameron << time_blocks[i]
-      i += 1
-      next output << 'C'
-    elsif overlap_any?(jamie, time_blocks[i]) && overlap_any?(cameron, time_blocks[i])
+  time_blocks.each do |activity|
+    if overlap_any?(jamie, activity) && overlap_any?(cameron, activity)
       break output = 'IMPOSSIBLE'
-    elsif overlap_any?(cameron, time_blocks[i])
-      jamie << time_blocks[i]
-      output << 'J'
-    elsif !overlap_any?(jamie, time_blocks[i])
-      jamie << time_blocks[i]
-      output << 'J'
-    elsif overlap_any?(jamie, time_blocks[i])
-      cameron << time_blocks[i]
+    elsif overlap_any?(jamie, activity)
       output << 'C'
+      cameron << activity
+    elsif overlap_any?(cameron, activity)
+      output << 'J'
+      jamie << activity
+    elsif !overlap_any?(jamie, activity)
+      output << 'J'
+      jamie << activity
+    elsif !overlap_any?(cameron, activity)
+      output << 'C'
+      cameron << activity
+    else
+      break output = 'IMPOSSIBLE'
     end
-    i += 1
- end
+  end
   "Case ##{test}: #{output}"
 end
+
+  # i = 0
+  # while i < time_blocks.length
+  #   if i == 0
+  #     cameron << time_blocks[i]
+  #     i += 1
+  #     next output << 'C'
+  #   elsif overlap_any?(jamie, time_blocks[i]) && overlap_any?(cameron, time_blocks[i])
+  #     break output = 'IMPOSSIBLE'
+  #   elsif overlap_any?(cameron, time_blocks[i])
+  #     jamie << time_blocks[i]
+  #     output << 'J'
+  #   elsif !overlap_any?(jamie, time_blocks[i])
+  #     jamie << time_blocks[i]
+  #     output << 'J'
+  #   elsif overlap_any?(jamie, time_blocks[i])
+  #     cameron << time_blocks[i]
+  #     output << 'C'
+  #   end
+  #   i += 1
+ # end
 
 # Gets input, and assigns it to variables
 # test_cases = gets.to_i
