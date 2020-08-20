c0f5f102a32499953cf1e827aac3ed50eaef70ee
jubaan <julio.ab@outlook.com>
2020-08-20 16:08:38 +0000

    remove byebug

--- challenges/parenting_partering_returns/parenting_partnering_returns.rb
+++ challenges/parenting_partering_returns/parenting_partnering_returns.rb
@@ -1,35 +1,34 @@
-require 'pry-byebug'
-
 def overlaps?(activity_1, activity_2)
-    return false unless activity_1
-    return false unless activity_2
-    [activity_1[0], activity_2[0]].max < [activity_1[-1], activity_2[-1]].min
+  return false unless activity_1
+  return false unless activity_2
+
+  [activity_1[0], activity_2[0]].max < [activity_1[-1], activity_2[-1]].min
 end
 
 def schedule(test, time_blocks)
-     cameron = []
-     jamie = []
-     output = ''
+  cameron = []
+  jamie = []
+  output = ''
 
-     i = 0
-     while i < time_blocks.length
-       # binding.pry
-        if i == 0
-            cameron << time_blocks[i]
-            i += 1
-            next output << 'C'
-        elsif overlaps?(cameron.last, time_blocks[i])  && overlaps?(jamie.last, time_blocks[i])
-            break output = 'IMPOSSIBLE'
-        elsif overlaps?(cameron.last, time_blocks[i])
-            jamie << time_blocks[i]
-            output << 'J'
-        else
-            cameron << time_blocks[i]
-            output << 'C'
-        end
-        i += 1
+  i = 0
+  while i < time_blocks.length
+    # binding.pry
+    if i == 0
+      cameron << time_blocks[i]
+      i += 1
+      next output << 'C'
+    elsif overlaps?(cameron.last, time_blocks[i]) && overlaps?(jamie.last, time_blocks[i])
+      break output = 'IMPOSSIBLE'
+    elsif overlaps?(cameron.last, time_blocks[i])
+      jamie << time_blocks[i]
+      output << 'J'
+    else
+      cameron << time_blocks[i]
+      output << 'C'
     end
-    "Case ##{test}: #{output}"
+    i += 1
+ end
+  "Case ##{test}: #{output}"
 end
 
 # Gets input, and assigns it to variables
