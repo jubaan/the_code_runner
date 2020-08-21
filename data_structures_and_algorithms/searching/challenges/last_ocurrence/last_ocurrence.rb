a394457bf810be37579a7ec8f43820dacc083496
jubaan <julio.ab@outlook.com>
2020-08-21 20:29:29 +0000

    Add doctest and fix returned value in last_occurce

--- data_structures_and_algorithms/searching/challenges/last_ocurrence/last_ocurrence.rb
+++ data_structures_and_algorithms/searching/challenges/last_ocurrence/last_ocurrence.rb
@@ -1,12 +1,18 @@
-# Retrieves, formats, and assigns the input
-size, value = gets.split(' ').map(&:to_i)
-arr = gets.split(' ').map(&:to_i)
+# doctest: linear_search - returns index of value's last appearance
+# >> linear_search(5, [4, 5, 4, 6, 1], 4)
+# => 3
 
-def linear_search(size, arr, value)
+# NOTE: list index starts from 1, rather than 0.
+
+# Retrieves, formats, and assigns the input
+# size, value = gets.split(' ').map(&:to_i)
+# arr = gets.split(' ').map(&:to_i)
+
+def last_ocurrence(size, arr, value)
   i = size
   while i <= size
     if arr[i - 1] == value
-      return i + 1
+      return i
     elsif i < 0
       return -1
     end
@@ -15,4 +21,4 @@ def linear_search(size, arr, value)
   -1
 end
 
-puts linear_search(size, arr, value)
+# puts linear_search(size, arr, value)
