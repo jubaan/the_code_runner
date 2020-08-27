accb0a07fd8afff053577b9458d34b6526ff275a
jubaan <julio.ab@outlook.com>
2020-08-27 21:20:17 +0000

    WIP: picking numbers

--- challenges/picking_numbers/picking_numbers.rb
+++ challenges/picking_numbers/picking_numbers.rb
@@ -1,5 +1,41 @@
-def picking_numbers(numbers)
 
+# doctest: returns a single integer denoting the maximum number of
+#          integers you can choose from the array
+# doctest: 4, 6, 5, 3, 3, 1
+# >> picking_numbers([4, 6, 5, 3, 3, 1])
+# => 3
+# doctest: 1, 2, 2, 3, 1, 2
+# >> picking_numbers([1, 2, 2, 3, 1, 2])
+# => 5
+# doctest: 1, 2, 3, 4, 3, 2, 1
+# >> picking_numbers([1, 2, 3, 4, 3, 2, 1])
+# => 7
+# doctest: 3, 2, 1, 4, 5, 6, 7, 8
+# >> picking_numbers([3, 2, 1, 4, 5, 6, 7, 8])
+# => 6
+
+def picking_numbers(numbers)
+  max = 0
+  numbers.each_index do |i|
+    left = numbers[..i].reverse
+    pivot = numbers[i]
+    right = numbers[i..]
+
+    count_left = length(left, pivot)
+    count_right = length(right, pivot)
+
+    size = count_left <=> count_right
+    triggers = { 1 => count_left, 0 => count_left, -1 => count_right }
+
+   max = triggers[size] if triggers[size] > max
+  end
+
+  max
+end
+
+def length(collection, pivot_value)
+  check_point = pivot_value
+  collection.each_with_index.select { |e, i| (e - check_point).abs <= 1 && check_point = collection[i] }.length
 end
 
 if __FILE__ == $PROGRAM_NAME # rubocop:disable Style/IfUnlessModifier
@@ -19,5 +55,4 @@ if defined? Minitest
     end
   end
   # require 'pry'
-  # binding.pry # rubocop:disable Lint/Debugger
 end
