f164758b152293a22bafb3393ffafd84ab56db90
jubaan <julio.ab@outlook.com>
2020-08-27 04:37:34 +0000

    WIP: start from scratch
    This does have test that we can use as we develop.

--- challenges/picking_numbers/picking_numbers.rb
+++ challenges/picking_numbers/picking_numbers.rb
@@ -1,16 +1,23 @@
 def picking_numbers(numbers)
-  max = 0
-  i = 0
-  while i < numbers.length
-    numbers.map { |integer| numbers.select { |m| (integer - m).abs <= 1 } }
-    size = numbers.select { |integer| (numbers[i] - integer).abs <= 1 }.count
-    max = size if size > max
 
-    i += 1
-  end
-  max
 end
 
-if __FILE__ == $PROGRAM_NAME
+if __FILE__ == $PROGRAM_NAME # rubocop:disable Style/IfUnlessModifier
   p picking_numbers([4, 6, 5, 3, 3, 1])
+end
+
+if defined? Minitest
+  describe 'picking numbers' do
+    it 'must return 3 for %s' % (data1 = [4, 6, 5, 3, 3, 1]).join(', ') do
+      _(picking_numbers(data1)).must_equal 3
+    end
+    it 'must return 3 for %s' % (data2 = [4, 5, 6, 3, 3, 1]).join(', ') do
+      _(picking_numbers(data2)).must_equal 3
+    end
+    it 'must return 6 for %s' % (data3 = [3, 2, 1, 4, 5, 6, 7, 8]).join(', ') do
+      _(picking_numbers(data3)).must_equal 6
+    end
+  end
+  # require 'pry'
+  # binding.pry # rubocop:disable Lint/Debugger
 end
