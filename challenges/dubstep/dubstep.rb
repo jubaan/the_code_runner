a18801f98396287b8182cd73b9cc95efc84066ee
jubaan <julio.ab@outlook.com>
2020-08-27 14:58:29 +0000

    WIP: dubstep refactor
    Change double quotes for single coutes.

--- challenges/dubstep/dubstep.rb
+++ challenges/dubstep/dubstep.rb
@@ -1,11 +1,5 @@
-# doctest: Returns a string with the initial song
-# >> song_decoder("AWUBBWUBC")
-# => "A B C"
-# >> song_decoder("AWUBWUBWUBBWUBWUBWUBC")
-# => "A B C"
-# >> song_decoder("WUBAWUBBWUBCWUB")
-# => "A B C"
-
 def song_decoder(song)
-  song.gsub(/(WUB)+/, " ").strip
+  song.gsub(/(WUB)+/, ' ').strip
 end
+
+song_decoder('WUBAWUBBWUBCWUB')
