fc379b95305f5dd7476270fc72d4c3c8ee9c10dc
jubaan <julio.ab@outlook.com>
2020-08-27 02:39:56 +0000

    WIP: wrong answer
    The problem is in the way select groups the subarrays

--- challenges/picking_numbers/picking_numbers.rb
+++ challenges/picking_numbers/picking_numbers.rb
@@ -1,16 +1,16 @@
-require 'pry-byebug'
-
-def pickingNumbers(a)
+def picking_numbers(numbers)
   max = 0
   i = 0
-  while i < a.length
-    a.map { |n| a.select { |m| (n - m).abs <= 1 }}
-    size = a.select { |n| (a[i] - n).abs <= 1}.count
+  while i < numbers.length
+    numbers.map { |integer| numbers.select { |m| (integer - m).abs <= 1 } }
+    size = numbers.select { |integer| (numbers[i] - integer).abs <= 1 }.count
     max = size if size > max
-    binding.pry
 
     i += 1
   end
+  max
 end
 
-pickingNumbers([4, 6, 5, 3, 3, 1])
+if __FILE__ == $PROGRAM_NAME
+  p picking_numbers([4, 6, 5, 3, 3, 1])
+end
